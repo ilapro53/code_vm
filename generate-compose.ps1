56979540eebc382597988dd1818bc4342a453053
Ilya <ilapro53@gmail.com>
2026-06-15 16:53:09 +0300

    fix: add C drive to docker-compose generation

--- generate-compose.ps1
+++ generate-compose.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = "Stop"
 $drives = Get-PSDrive -PSProvider FileSystem | Where-Object {
     $_.Free -gt 0 -and
     $_.Root -notmatch '^\\' -and
-    $_.Name -notin @('A', 'C', 'O')
+    $_.Name -notin @('A', 'O')
 } | Sort-Object Name
 
 # Build volumes block
